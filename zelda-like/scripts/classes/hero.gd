3d1139a37d27b44334894d0310d1b3ceea7b2711
Rafael Correia <rafaeljpc@gmail.com>
2016-08-22 15:24:10 -0300

    Melee Area2D

--- zelda-like/scripts/classes/hero.gd
+++ zelda-like/scripts/classes/hero.gd
@@ -1,2 +1,21 @@
 
 extends "res://zelda-like/scripts/classes/monster.gd"
+
+const DIR = preload("res://zelda-like/scripts/utility/directions.gd")
+
+onready var sprite = get_node("sprite")
+
+func _move_to(dir):
+  ._move_to(dir)
+  if direction == DIR.UP:
+    set_rotd(180)
+    sprite.set_rotd(-180)
+  elif direction == DIR.DOWN:
+    set_rotd(0)
+    sprite.set_rotd(0)
+  elif direction == DIR.LEFT:
+    set_rotd(270)
+    sprite.set_rotd(-270)
+  elif direction == DIR.RIGHT:
+    set_rotd(90)
+    sprite.set_rotd(-90)
