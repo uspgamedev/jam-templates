fd6e8f516dfb6feaf45960b759f13b3d1115fe1d
Rafael Correia <rafaeljpc@gmail.com>
2016-08-22 16:13:39 -0300

    Melee Damage

--- zelda-like/scripts/classes/hero.gd
+++ zelda-like/scripts/classes/hero.gd
@@ -2,8 +2,10 @@
 extends "res://zelda-like/scripts/classes/monster.gd"
 
 const DIR = preload("res://zelda-like/scripts/utility/directions.gd")
+const MONSTER = preload("res://zelda-like/scripts/classes/monster.gd")
 
 onready var sprite = get_node("sprite")
+onready var hitbox = get_node("hitbox")
 
 func _move_to(dir):
   ._move_to(dir)
@@ -19,3 +21,13 @@ func _move_to(dir):
   elif direction == DIR.RIGHT:
     set_rotd(90)
     sprite.set_rotd(-90)
+
+func _act(act):
+  printt("act=", act)
+  if act == 0:
+    var range_bodies = hitbox.get_overlapping_bodies()
+    printt("bodies=", range_bodies)
+    for body in range_bodies:
+      if body extends MONSTER:
+        printt("monster=", body)
+        body._take_dmg(body, body.get_collision_normal(), 1)
