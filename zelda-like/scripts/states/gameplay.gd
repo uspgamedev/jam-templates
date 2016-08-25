3ba7bd1bfa5a626263f126f7ac8a75d2f09c9161
Rafael Correia <rafaeljpc@gmail.com>
2016-08-25 16:59:40 -0300

    Monster die when damage taken

--- zelda-like/scripts/states/gameplay.gd
+++ zelda-like/scripts/states/gameplay.gd
@@ -10,16 +10,13 @@ func _ready():
     input.connect("hold_direction", self._hero, "_move_to")
     input.connect("press_quit", self, "_quit")
     input.connect("press_action", self._hero, "_act")
+    self._hero.connect("died", self, "_quit")
     set_process(true)
 
 func _start_camera():
     var cam = Camera2D.new()
     self._hero.add_child(cam)
     cam.make_current()
-
-func _process(delta):
-    if self._hero.damage >= self._hero.maxHP:
-        get_tree().quit()
 
 func _quit():
     get_tree().quit()
